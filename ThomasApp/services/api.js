1d33d99bf470b96d3a17a2536d7fe5ce1717a4cd
CThomas <corde.thomas1@gmail.com>
2026-01-28 13:59:27 +0100

    docs: ReadME

--- ThomasApp/services/api.js
+++ ThomasApp/services/api.js
@@ -1,12 +1,13 @@
-const API_KEY = '14eab7ac5e6cb898f642faf692738212';
-const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=fr-FR`;
+import { TMDB_API_KEY, TMDB_BASE_URL } from '@env';
+
+const API_URL = `${TMDB_BASE_URL}/movie/popular?api_key=${TMDB_API_KEY}&language=fr-FR`;
 
 useEffect(() => {
-fetch(API_URL)
-    .then(response => response.json())
-    .then(data => {
-    console.log(data.results); 
-    setMovies(data.results);
-}   )
-    .catch(error => console.error(error));
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.results);
+      setMovies(data.results);
+    })
+    .catch((error) => console.error(error));
 }, []);
